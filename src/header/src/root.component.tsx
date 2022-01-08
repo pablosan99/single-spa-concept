4fe526e0739ab16ca47a8f3111171ca91ade53a4
Paweł Nowak <pawelpnowak@outlook.com>
2022-01-08 17:15:02 +0100

    Build production and copy-all

--- src/header/src/root.component.tsx
+++ src/header/src/root.component.tsx
@@ -2,7 +2,7 @@ const Root = (props) => {
   return (
     <section>
       <div>HEADER {props.name} is mounted!</div>
-      <div className="className">test message in header</div>
+      <div className="className">test message in header !!! ASDASDSAD</div>
     </section>
   );
 };
